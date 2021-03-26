f2e3b8dc7b2dfdf88a0050c9c70a65d029058ad1
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2021-03-26 08:45:17 -0300

    finished some test and an openning informartion on server

---
+++ hacker-chat/server/src/constants.js
@@ -0,0 +1,8 @@
+export const constants = {
+  event: {
+    MESSAGE: 'message',
+    NEW_USER_CONNECTED: 'newUserConnected',
+    DISCONNECT_USER: 'diconnectUser',
+    UPDATE_USERS: 'updateUsers'
+  }
+}
